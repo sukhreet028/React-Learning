31158411e02a2ca83565e77fdd073df36cd78e90
Sukhvir2104 <sukhvir21@live.com>
2021-10-15 18:11:55 -0400

    improvements in calculator

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,147 +1,108 @@
 import './Cal.css';
-import Input1 from '../input1-com/Input1'
+import Input1 from '../input1-com/Input1';
 import Buttons from '../buttons/buttons';
 import { useState } from 'react';
 
 function Cal() {
-    const [result, setResult] = useState(0);
-    const buttonArray = [9,8,7,6,5,4,3,2,1,0,'.'];
-    // const operator=['+','-','*','/','%']
-    const  [first,setFirst] = useState([]);
-    const  [storeFirstValue , setStoreFirstValue] = useState();
-    let operator;
-    
-    const sumData = (value) => {
-      if(value === 'sum')
-       {
-        let storeSecondValue = parseInt(first.join(''));
-        let sum = (storeFirstValue ? storeFirstValue : 0) + storeSecondValue;
-        console.log(storeFirstValue, storeSecondValue);
-        setResult(sum);
-        console.log(sum);
-        setFirst([]);
-      }else if(value === '+') {
+  const [result, setResult] = useState(0);
+  const [operator, setOperator] = useState();
+  const buttonArray = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, '.'];
+  const operatorArray = ['+', '-', '*', '/', '%'];
+  const [first, setFirst] = useState([]);
+  const [storeFirstValue, setStoreFirstValue] = useState();
+  let sum, min, div, mul;
+
+  const sumData = (value) => {
+    if (operator && operatorArray.includes(value)) {
+      if (operator === '+') {
+        // let storeSecondValue = parseInt(first.join(''));
+        sum = (storeFirstValue ? storeFirstValue : 0) + result;
+      } else if (operator === '-') {
+        sum = (storeFirstValue ? storeFirstValue : 0) - result;
+      }
+
+      setStoreFirstValue(sum);
+      console.log('iside minus ' + storeFirstValue, result, value);
+      setFirst([]);
+      setResult(sum);
+      setOperator(value);
+    } else if (value === '=') {
+      let storeSecondValue = parseInt(first.join(''));
+      if (operator === '+') {
+        sum = (storeFirstValue ? storeFirstValue : 0) + storeSecondValue;
+      } else if (operator === '-') {
+        sum = (storeFirstValue ? storeFirstValue : 0) - storeSecondValue;
+      }
+
+      console.log('sum func' + storeFirstValue, storeSecondValue);
+      setResult(sum);
+      setOperator(null);
+      setStoreFirstValue(sum);
+      console.log(sum);
+      setFirst([]);
+    } else if (operatorArray.includes(value) && !operator) {
+      setOperator(value);
+      if (first.length > 0) {
         setStoreFirstValue(parseInt(first.join('')));
-        setFirst([]);
-      } else {
-        first.push(value);
-        setFirst(prev =>[...prev]);
-        let keys = parseInt(first.join(''));
-        setResult(keys);
       }
+      console.log('first' + storeFirstValue);
+      setFirst([]);
+    } else {
+      first.push(value);
+      setFirst((prev) => [...prev]);
+      let keys = parseInt(first.join(''));
+      setResult(keys);
     }
-    const minData = (value) => {
-        if(value === 'min')
-         {
-          let storeSecondValue = parseInt(first.join(''));
-          let min = (storeFirstValue ? storeFirstValue : 0) - storeSecondValue;
-          console.log(storeFirstValue, storeSecondValue);
-          setResult(min);
-          setFirst([]);
-        }else if(value === '-') {
-          setStoreFirstValue(parseInt(first.join('')));
-          setFirst([]);
-        } else {
-          first.push(value);
-          setFirst(prev =>[...prev]);
-          let keys = parseInt(first.join(''));
-          setResult(keys);
-        }
-      }
-      const mulData = (value) => {
-        if(value === 'mul')
-         {
-          let storeSecondValue = parseInt(first.join(''));
-          let mul = (storeFirstValue ? storeFirstValue : 0) * storeSecondValue;
-          console.log(storeFirstValue, storeSecondValue);
-          setResult(mul);
-          setFirst([]);
-        }else if(value === '*') {
-          setStoreFirstValue(parseInt(first.join('')));
-          setFirst([]);
-        } else {
-          first.push(value);
-          setFirst(prev =>[...prev]);
-          let keys = parseInt(first.join(''));
-          setResult(keys);
-        }
-      }
-      const divData = (value) => {
-        if(value === 'div')
-         {
-          let storeSecondValue = parseInt(first.join(''));
-          let div = (storeFirstValue ? storeFirstValue : 0) / storeSecondValue;
-          console.log(storeFirstValue, storeSecondValue);
-          setResult(div);
-          setFirst([]);
-        }else if(value === '/') {
-          setStoreFirstValue(parseInt(first.join('')));
-          setFirst([]);
-        } else {
-          first.push(value);
-          setFirst(prev =>[...prev]);
-          let keys = parseInt(first.join(''));
-          setResult(keys);
-        }
-      }
-      function equalOperator (){
-        if(sumData('+') && operator==='='){
-            setResult(sum);
-        }else if(sumData('-') && operator==='='){
-            setResult(min);
-        }else if(sumData('/') && operator==='='){
-            setResult(div);
-          }else if(sumData('*') && operator==='='){
-            setResult(mul);
-          }else{
-              console.log('no operator');
-          }}
-    return (
-      <div className='container'>
-        <Input1 result={result} />
-        {buttonArray.map((data) =>(<Buttons
+  };
+  return (
+    <div className="container">
+      <Input1 result={result} />
+      {buttonArray.map((data) => (
+        <Buttons
           key={data}
           buttonType="button"
           name={data}
           classStyle="cal-button"
-          func={()=>minData(data)}
-        />))}
-         <Buttons
-          buttonType="button"
-          name="="
-          classStyle="cal-button"
-          func={()=>equalOperator('=')}
-        /> 
-          <Buttons
-          buttonType="button"
-          name="+"
-          classStyle="cal-button"
-          func={()=>sumData('sum')}
+          func={() => sumData(data)}
         />
-         <Buttons
-          buttonType="button"
-          name="-"
-          classStyle="cal-button"
-          func={()=>minData('-')}/>
-          <Buttons
-          buttonType="button"
-          name="*"
-          classStyle="cal-button"
-          func={()=>mulData('*')}/>
-           {/* <Buttons
+      ))}
+      <Buttons
+        buttonType="button"
+        name="="
+        classStyle="cal-button"
+        func={() => sumData('=')}
+      />
+      <Buttons
+        buttonType="button"
+        name="+"
+        classStyle="cal-button"
+        func={() => sumData('+')}
+      />
+      <Buttons
+        buttonType="button"
+        name="-"
+        classStyle="cal-button"
+        func={() => sumData('-')}
+      />
+      <Buttons
+        buttonType="button"
+        name="*"
+        classStyle="cal-button"
+        func={() => sumData('*')}
+      />
+      {/* <Buttons
           buttonType="button"
           name="="
           classStyle="cal-button"
           func={()=>divData('div')}
         /> */}
-          <Buttons
-          buttonType="button"
-          name="/"
-          classStyle="cal-button"
-          func={()=>divData('/')}/>
-    
-        
-      </div>
-    );
-  }
-  export default Cal;
+      <Buttons
+        buttonType="button"
+        name="/"
+        classStyle="cal-button"
+        func={() => sumData('/')}
+      />
+    </div>
+  );
+}
+export default Cal;
